051b3efde39d4eaeff652b5ad51148d62ad2f44d
Herson Cruz <herson@datolab.com>
2022-05-20 08:06:57 -0600

    adding maps and fetchjson projects

---
+++ maps/src/index.ts
@@ -0,0 +1,13 @@
+//import { User } from './User'
+//import { Company } from './Company'
+
+const map = new google.maps.Map(
+  document.getElementById("map") as HTMLElement,
+  {
+    zoom: 1,
+    center: {
+      lat: 0,
+      lng: 0,
+    }
+  }
+);
