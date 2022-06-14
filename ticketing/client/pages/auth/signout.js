a48db11d9fe139e2eecf2eba04d41f53129ceb73
Herson Cruz <herson@datolab.com>
2022-06-13 22:36:14 -0600

    client auth completed

---
+++ ticketing/client/pages/auth/signout.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import useRequest from '../../hooks/use-request';
+
+export default () => {
+  const { doRequest } = useRequest({
+    url: '/api/users/signout',
+    method: 'get',
+    body: {},
+    onSuccess: () => Router.push('/')
+  });
+
+  useEffect(() => {
+    doRequest();
+  }, []);
+  return <div>Singning you out...</div>
+};
---
+++ ticketing/client/pages/index.js
@@ -0,0 +1,20 @@
+import buildClient from '../api/build-client';
+
+const Landing = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
+};
+
+Landing.getInitialProps = async (context) => {
+//  const response = await axios.get('/api/users/currentuser');
+//  return response.data;
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default Landing;
